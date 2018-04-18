2461b4fab8985430356cd2769fb295534f388e0a
Hilmi Raditya <33018956+hilmiraditya@users.noreply.github.com>
2018-04-18 19:00:05 +0700

    Nyoba
    Coba liat home-nya

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Padang Pantek
+                    Bau Memek
                 </div>
 
                 <div class="links">
