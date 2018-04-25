465edf5f65dc94ea85ef7b83c20a4a1df1e8eafa
mailsyarief <ismail8syarief@gmail.com>
2018-04-26 00:42:07 +0700

    beresin view + pasang custom bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Padang Pantek
                 </div>
 
                 <div class="links">
