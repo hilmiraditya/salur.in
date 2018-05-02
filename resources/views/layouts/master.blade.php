e2ce0e6f915ad2e414bbc1baaac8148e543967eb
Hilmi Raditya <raditya113@icloud.com>
2018-05-02 18:19:14 +0700

    Benerin About

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
 
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="shortcut icon" href="{{ url('images/logo2.png') }}" />
     <link href="{{ url('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ url('css/shop-homepage.css') }}" rel="stylesheet">
     <script type="text/javascript" src="{{ url('js/jquery-3.3.1.js') }}"></script>
