3e1a91a9712b57130e53e88390c79d36308f7f81
mailsyarief <ismail8syarief@gmail.com>
2018-05-09 13:03:52 +0700

    tambah page about

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tentang', function () {
+    return view('about');
+});
+
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
